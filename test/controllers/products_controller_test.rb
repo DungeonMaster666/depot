b7dd5ac0eef95892c6302228ab6afeb50c9e2dab
DungeonMaster666 <m.grishajevs@gmail.com>
2021-12-06 12:47:49 +0200

    4. and 5. task

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,6 +17,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   end
 
+
+
   test "should get new" do
     get new_product_url
     assert_response :success
@@ -43,6 +45,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
+  end
+
+  ## ---------------------
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count',0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
