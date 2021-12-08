ab6b1f142d7cca396b20b407ebf13a3755d2a365
DungeonMaster666 <m.grishajevs@gmail.com>
2021-12-08 18:33:02 +0200

    14.chapter

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -10,33 +10,6 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Orders"
   end
 
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Create Order"
-
-    assert_text "Order was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Order" do
-    visit orders_url
-    click_on "Edit", match: :first
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Update Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
-  end
 
   test "destroying a Order" do
     visit orders_url
@@ -46,4 +19,72 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_text "Order was successfully destroyed"
   end
+
+  test "add to cart" do
+    visit store_index_url
+    assert_no_selector "h2", text: "Your Cart"
+    click_on 'Add to Cart', match: :first
+    assert_selector "h2", text: "Your Cart"
+  end
+
+  test 'hide cart' do
+    visit store_index_url
+    assert_no_selector "h2", text: "Your Cart"
+    click_on 'Add to Cart', match: :first
+    assert_selector "h2", text: "Your Cart"
+    page.accept_confirm do
+      click_on 'Empty cart', match: :first
+    end
+    assert_no_selector "h2", text: "Your Cart"
+
+  end
+
+  test "check routing number" do
+    visit store_index_url
+    click_on 'Add to Cart', match: :first
+    click_on 'Checkout'
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+    assert_no_selector "#order_account_number"
+    assert_no_selector "#order_routing_number"
+    select 'Check', from: 'Pay type'
+    assert_selector "#order_routing_number"
+    assert_selector "#order_account_number"
+
+  end
+
+  test "credit card" do
+    visit store_index_url
+    click_on 'Add to Cart', match: :first
+    click_on 'Checkout'
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+    assert_no_selector "#order_credit_card_number"
+    assert_no_selector "#order_expiration_date"
+
+    select 'Credit card', from: 'Pay type'
+    assert_selector "#order_credit_card_number"
+    assert_selector "#order_expiration_date"
+
+  end
+
+  test "Purchase" do
+    visit store_index_url
+    click_on 'Add to Cart', match: :first
+    click_on 'Checkout'
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+    assert_no_selector "#order_po_number"
+
+    select 'Purchase order', from: 'Pay type'
+    assert_selector "#order_po_number"
+
+  end
+
+
+
 end
+
